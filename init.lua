9a15873b5ab418a26aaa70accf01ac82cbd3893b
Rafail <rafailadam46@gmail.com>
2025-05-25 20:12:07 +0300

    fixed mason, added jedi instead of pyright

--- init.lua
+++ init.lua
@@ -2,3 +2,4 @@ vim.g.mapleader = " "
 require("options")
 require("plugins")
 require("plugin_options")
+require("diagnostics")
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -19,6 +19,7 @@ require("lazy").setup({
 			vim.cmd.colorscheme("kanagawa-wave")
 		end,
 	},
+
 	{
 		"nvim-treesitter/nvim-treesitter",
 		dependencies = {
@@ -44,10 +45,8 @@ require("lazy").setup({
 				textobjects = {
 					select = {
 						enable = true,
-
 						-- Automatically jump forward to textobj, similar to targets.vim
 						lookahead = true,
-
 						keymaps = {
 							["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
 							['iee'] = '@assignment.inner',
@@ -109,43 +108,52 @@ require("lazy").setup({
 			})
 		end,
 	},
+
 	{
 		"nvim-treesitter/nvim-treesitter-textobjects",
 	},
+
 	{
 		"neovim/nvim-lspconfig",
 	},
+
 	{
-		"folke/which-key.nvim"
+		"folke/which-key.nvim",
+		opts = {},
 	},
+
 	{
 		"windwp/nvim-autopairs",
 		event = "InsertEnter",
 		config = true,
 	},
+
 	{
 		"nvim-lualine/lualine.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" }
 	},
+
 	{
 		"tpope/vim-fugitive",
 		"tpope/vim-rhubarb",
+		opts = {},
 	},
+
 	{
-		"lewis6991/gitsigns.nvim"
+		"lewis6991/gitsigns.nvim",
+		opts = {}
 	},
+
 	{
 		'saghen/blink.cmp',
 		-- optional: provides snippets for the snippet source
-		dependencies = { 'rafamadriz/friendly-snippets' },
-
+		dependencies = { 'rafamadriz/friendly-snippets', "L3MON4D3/LuaSnip" },
 		-- use a release tag to download pre-built binaries
 		version = '1.*',
 		-- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
 		-- build = 'cargo build --release',
 		-- If you use nix, you can build from source using latest nightly rust with:
 		-- build = 'nix run .#build-plugin',
-
 		---@module 'blink.cmp'
 		---@type blink.cmp.Config
 		opts = {
@@ -153,31 +161,25 @@ require("lazy").setup({
 			-- 'super-tab' for mappings similar to vscode (tab to accept)
 			-- 'enter' for enter to accept
 			-- 'none' for no mappings
-			--
 			-- All presets have the following mappings:
 			-- C-space: Open menu or open docs if already open
 			-- C-n/C-p or Up/Down: Select next/previous item
 			-- C-e: Hide menu
 			-- C-k: Toggle signature help (if signature.enabled = true)
-			--
 			-- See :h blink-cmp-config-keymap for defining your own keymap
 			keymap = { preset = 'default' },
-
 			appearance = {
 				-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
 				-- Adjusts spacing to ensure icons are aligned
 				nerd_font_variant = 'mono'
 			},
-
 			-- (Default) Only show the documentation popup when manually triggered
 			completion = { documentation = { auto_show = false } },
-
 			-- Default list of enabled providers defined so that you can extend it
 			-- elsewhere in your config, without redefining it, due to `opts_extend`
 			sources = {
 				default = { 'lsp', 'path', 'snippets', 'buffer' },
 			},
-
 			-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
 			-- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
 			-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
@@ -187,10 +189,12 @@ require("lazy").setup({
 		},
 		opts_extend = { "sources.default" }
 	},
+
 	{
 		"mason-org/mason.nvim",
 		opts = {}
 	},
+
 	{
 		"mason-org/mason-lspconfig.nvim",
 		opts = {},
