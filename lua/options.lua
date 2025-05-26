0200f6f127f248cb9778a94d38fbb6ecc480841b
Rafail <rafailadam46@gmail.com>
2025-05-27 00:47:17 +0300

    remaps for git and autocomplete

--- lua/options.lua
+++ lua/options.lua
@@ -40,3 +40,14 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = 'Move highlighted lines up
 vim.keymap.set("n", "<leader>d", ':edit /Volumes/Mac/Users/rafailadam/Downloads/<CR>', { desc = 'Returns to Downloads directory' })
 vim.keymap.set("n", "<leader>t", ':tabnew<CR>', {desc = 'Opens new tab'})
 vim.keymap.set("n", "<leader>c", ':terminal<CR>', {desc = 'Opens terminal'})
+
+--Git
+vim.keymap.set("n", '<leader>gc', ":Git commit<CR>", { desc = '[C]ommit changes' })
+vim.keymap.set("n", '<leader>gl', ":Git log<CR>", { desc = 'Git [L]og' })
+vim.keymap.set("n", '<leader>gt', ":Git log --graph --pretty=format:'%h -%d %s (%cr) <%an>' --abbrev-commit --date=relative --all<CR>", { desc = 'Git [L]og [T]ree Full' })
+vim.keymap.set("n", '<leader>gs', ":Git status --ignored<CR>", { desc = 'Git [S]tatus' })
+vim.keymap.set("n", '<leader>gi', ":Git init .<CR>", { desc = 'Git [I]nitialize' })
+vim.keymap.set("n", '<leader>grv', ":Git remote -v<CR>", { desc = 'Git [R]emote -[V]' })
+vim.keymap.set("n", '<leader>gp', ":Git push ", { desc = 'Git [P]ush' })
+vim.keymap.set("n", '<leader>ga', ":Git add ", { desc = 'Git [A]dd' })
+vim.keymap.set("n", '<leader>gb', ":Git branch<CR>:Git checkout ", { desc = 'Git [B]ranch' })
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -157,17 +157,12 @@ require("lazy").setup({
 		---@module 'blink.cmp'
 		---@type blink.cmp.Config
 		opts = {
-			-- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-			-- 'super-tab' for mappings similar to vscode (tab to accept)
-			-- 'enter' for enter to accept
-			-- 'none' for no mappings
-			-- All presets have the following mappings:
-			-- C-space: Open menu or open docs if already open
-			-- C-n/C-p or Up/Down: Select next/previous item
-			-- C-e: Hide menu
-			-- C-k: Toggle signature help (if signature.enabled = true)
-			-- See :h blink-cmp-config-keymap for defining your own keymap
-			keymap = { preset = 'default' },
+			keymap = { 
+				preset = 'none',
+				['<Up>'] = { 'select_prev', 'fallback' },
+				['<Down>'] = { 'select_next', 'fallback' },
+				['<Tab>'] = { 'select_and_accept' },
+			},
 			appearance = {
 				-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
 				-- Adjusts spacing to ensure icons are aligned
@@ -202,6 +197,14 @@ require("lazy").setup({
 			{"mason-org/mason.nvim", opts = {}},
 			"neovim/nvim-lspconfig",
 		},
+	},
+	{
+		'echasnovski/mini.nvim',
+		version = '*',
+	},
+	{
+		'echasnovski/mini.jump2d',
+		version = '*',
 	},
 })
 
